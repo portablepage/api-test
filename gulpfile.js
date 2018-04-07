662d2de7174875e6d9902afa3423160f9d272945
portablepage <31555211+portablepage@users.noreply.github.com>
2018-04-08 01:27:27 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ gulp.task('combine-json', function () {
 	
 	gulp.src('./data/*.json')
 		.pipe(concat_json("./config/pages.json"))
-		.pipe(gulp.dest('./data/'));
+		.pipe(gulp.dest('./site/'));
 
 });
 
@@ -36,9 +36,9 @@ gulp.task('copy-admin', function () {
 	gulp.src(['./admin/**/*']).pipe(gulp.dest('./site/admin'));
 });
 
-//gulp.task('copy-data', function () {
-	//gulp.src(['./data/**/*']).pipe(gulp.dest('./site/data'));
-//});
+gulp.task('copy-data', function () {
+	gulp.src(['./data/**/*']).pipe(gulp.dest('./site/data'));
+});
 
 gulp.task('copy-layouts', function () {
 	gulp.src(['./layouts/**/*']).pipe(gulp.dest('./site/layouts'));
