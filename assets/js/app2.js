90a07c28ee8e21c5645f1a33e74fa4bf2c2c9bd7
portablepage <31555211+portablepage@users.noreply.github.com>
2018-04-07 02:22:43 +0200

    Alternative approach

---
+++ assets/js/app2.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+	
+	// this is needed for logging in
+	var $_GET = {};
+	document.location.search.replace(/\??(?:([^=]+)=([^&]*)&?)/g, function () {
+		function decode(s) {
+			return decodeURIComponent(s.split("+").join(" "));
+		}
+
+		$_GET[decode(arguments[1])] = decode(arguments[2]);
+	});
+
+	var token = $_GET['access_token'];
+	if (typeof token !== 'undefined') {
+		//alert(token);
+		
+		sessionStorage.setItem('token', token);
+		
+		// insert the CMS
+		$('#app').html('<link href="https://api.dashpilot.com/v1/css/" rel="stylesheet"><script src="https://api.dashpilot.com/v1/js/?access_token='+token+'"></script>');
+		
+	}
+
+	
+});
+		
+	
+
+		function get_page(){
+			
+			var page = window.location.hash.substr(1).replace('/', ''); // set globally
+			
+			if(page==''){
+			   page = 'index';
+			}
+			
+			return page;
+			
+		}
+
+	
