3f9185ce9ec7a0d101c37a01cab71a3b02955bcc
portablepage <31555211+portablepage@users.noreply.github.com>
2018-04-07 02:49:22 +0200

    Update app2.js

--- assets/js/app2.js
+++ assets/js/app2.js
@@ -24,6 +24,8 @@ $(document).ready(function(){
 	
 	if (sessionStorage.getItem('token') !== null) {
 	// insert the CMS
+		
+	var token = sessionStorage.getItem('token');
 	$('#app').html('<link href="https://api.dashpilot.com/v1/css/" rel="stylesheet"><script src="https://api.dashpilot.com/v1/js/?access_token='+token+'"></script>');
 	}
 	
